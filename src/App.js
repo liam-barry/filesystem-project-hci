e6bf9c034245d32f7e4fed4e13d4fe7fc577b264
Reid Maury <reidmaury@MacBook-Air-100.local>
2026-02-19 20:32:47 -0500

    adding figma html/css files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function CD() {
+  return (
+    <img
+      src="icons/cd.png"
+      alt="CD icon"
+    />
+  );
+}
+function CD_Rack() {
+  return (
+    <img
+      src="icons/cd_rack.png"
+      alt="CD Rack icon"
+    />
+  );
+}
+function Rack_spinner() {
+  return (
+    <img
+      src="icons/rack_spinner.png"
+      alt="Rack spinner icon"
+    />
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CD />
+        <CD_Rack />
+        <Rack_spinner />
       </header>
     </div>
   );
 }
 
-export default App;
+ export default App;
+
